7a98fe073379790460f0ed4dd95bd10fe90dcc38
Brandon Robbins <brandonr@nccpa.net>
2021-11-16 17:54:22 -0500

    initial commit

---
+++ MvcApp.Common/IVirusScanner.cs
@@ -0,0 +1,61 @@
+﻿using Cloudmersive.APIClient.NETCore.VirusScan.Api;
+using Cloudmersive.APIClient.NETCore.VirusScan.Client;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MvcApp.Common
+{
+    public interface IVirusScanner
+    {
+        void Scan(Stream file);
+        Task ScanAsync(Stream file);
+    }
+
+    public class VirusScanner : IVirusScanner
+    {
+        #region Constructor
+
+        public VirusScanner(string apiKey)
+        {
+            this.ScanApi = new Lazy<ScanApi>(() =>
+            {
+                return new ScanApi(new Configuration()
+                {
+                    ApiKey = new Dictionary<string, string>() { { CultureInfo.CurrentCulture.TextInfo.ToTitleCase(nameof(Configuration.ApiKey)), apiKey } }
+                });
+            });
+        }
+
+        #endregion
+
+        #region Properties
+
+        public Lazy<ScanApi> ScanApi { get; private set; }
+
+        #endregion
+
+        public static IVirusScanner Default(string apiKey) => new VirusScanner(apiKey);
+
+        public void Scan(Stream file)
+        {
+            var result = this.ScanApi.Value.ScanFile(file);
+
+            // parse the result
+            if (result.CleanResult != true)
+                throw new Exception("File is infected.");
+
+        }
+
+        public async Task ScanAsync(Stream file)
+        {
+            var result = await this.ScanApi.Value.ScanFileAsync(file);
+
+            // parse the result
+            if (result.CleanResult != true)
+                throw new Exception("File is infected.");
+        }
+    }
+}
---
+++ MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using MvcApp.Common;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index() => View();
+
+        [HttpPost]
+        public ActionResult Scan(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                // i was too lazy to add dependency injection... this will have to work for now
+                var scanner = VirusScanner.Default(ConfigurationManager.AppSettings["VirusScanner:ApiKey"]);
+                // scan and nothing happens
+                scanner.Scan(file.InputStream);
+                // notification
+                TempData.Add("ScanResult", "File scanned successfully");
+            }
+
+            return View("Index");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ScanAsync(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                // i was too lazy to add dependency injection... this will have to work for now
+                var scanner = VirusScanner.Default(ConfigurationManager.AppSettings["VirusScanner:ApiKey"]);
+                // scan and nothing happens
+                await scanner.ScanAsync(file.InputStream);
+                // notification
+                TempData.Add("ScanResult", "File scanned successfully");
+            }
+
+            return View("Index");
+        }
+    }
+}
